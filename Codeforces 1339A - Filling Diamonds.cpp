15211feaa312daa6832b58aa9e6bb8a4bbcb3c2e
zalfa <92864261+ZalfaNafila@users.noreply.github.com>
2022-07-27 14:40:28 +0700

    Humm :0
    https://codeforces.com/problemset/problem/1339/A

---
+++ Codeforces 1339A - Filling Diamonds.cpp
@@ -0,0 +1,17 @@
+//https://codeforces.com/problemset/problem/1339/A
+//ZalfaNafila C++
+
+#include <bits/stdc++.h>
+using namespace std;
+#define hi ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#include <cctype>
+
+int main(){
+	hi
+    int n,a;
+    cin >> n;
+    for(int i = 1; i<=n; i++){
+        cin >> a;
+        cout << a << endl;
+    }
+}
